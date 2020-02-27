9ead7bf6fc5ce03ec4820354f9dd1e4001652608
Alexander Do <alexanderdo@ADos-MBP-2019.local>
2020-02-26 22:46:54 -0800

    added another method

--- week_4/ps4a.py
+++ week_4/ps4a.py
@@ -196,7 +196,7 @@ def calculateHandlen(hand):
     hand: dictionary (string-> int)
     returns: integer
     """
-    # TO DO... <-- Remove this comment when you code this function
+    return sum(hand.values())
 
 
 
